8d10363e569313e144fb8b6346cc1bc51382015c
Folkex Design <info@folkex.com>
2018-05-08 15:40:18 +0300

    changes 5-8-2018
    pages design

---
+++ views/leftmenu.php
@@ -0,0 +1,41 @@
+<!-- BEGIN: Left Aside -->
+<button class="m-aside-left-close  m-aside-left-close--skin-dark " id="m_aside_left_close_btn"><i class="la la-close"></i></button>
+
+<div id="m_aside_left" class="m-grid__item	m-aside-left  m-aside-left--skin-dark ">	
+	<!-- BEGIN: Aside Menu -->
+	<div 
+		id="m_ver_menu" 
+		class="m-aside-menu  m-aside-menu--skin-dark m-aside-menu--submenu-skin-dark " 
+		m-menu-vertical="1"
+		 m-menu-scrollable="0" m-menu-dropdown-timeout="500"  
+		>		
+		<ul class="m-menu__nav  m-menu__nav--dropdown-submenu-arrow ">
+			<li class="m-menu__item  m-menu__item--active" aria-haspopup="true" >
+				<button type="button" class="btn m-btn m-btn--gradient-from-brand m-btn--gradient-to-info" id="button_not">Send Push Notification</button>
+			</li>
+			<li class="m-menu__section">
+                <i class="m-menu__section-icon flaticon-more-v3"></i>
+            </li>
+            <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true"  m-menu-submenu-toggle="hover"><a href="#" class="m-menu__link m-menu__toggle"><i class="m-menu__link-icon fa fa-th"></i><span class="m-menu__link-text">Summary</span></a><div class="m-menu__submenu "><span class="m-menu__arrow"></span>
+           </li>
+           <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true"  m-menu-submenu-toggle="hover"><a href="#" class="m-menu__link m-menu__toggle"><i class="m-menu__link-icon fa fa-pie-chart"></i><span class="m-menu__link-text">Reports</span></a><div class="m-menu__submenu "><span class="m-menu__arrow"></span>
+           </li>
+           <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true"  m-menu-submenu-toggle="hover"><a href="#" class="m-menu__link m-menu__toggle"><i class="m-menu__link-icon fa fa fa-rss"></i><span class="m-menu__link-text">RSS-to-push</span></a><div class="m-menu__submenu "><span class="m-menu__arrow"></span>
+           </li>
+           <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true"  m-menu-submenu-toggle="hover"><a href="#" class="m-menu__link m-menu__toggle"><i class="m-menu__link-icon fa fa-paper-plane"></i><span class="m-menu__link-text">Sent Notifications</span></a><div class="m-menu__submenu "><span class="m-menu__arrow"></span>
+           </li>
+            <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true"  m-menu-submenu-toggle="hover"><a href="#" class="m-menu__link m-menu__toggle"><i class="m-menu__link-icon fa fa-hourglass-half"></i><span class="m-menu__link-text">Scheduled Notifications</span></a><div class="m-menu__submenu "><span class="m-menu__arrow"></span>
+           </li>
+           <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true"  m-menu-submenu-toggle="hover"><a href="#" class="m-menu__link m-menu__toggle"><i class="m-menu__link-icon fa fa-bullhorn"></i><span class="m-menu__link-text">Triggered Campaigns</span></a><div class="m-menu__submenu "><span class="m-menu__arrow"></span>
+           </li>
+           <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true"  m-menu-submenu-toggle="hover"><a href="#" class="m-menu__link m-menu__toggle"><i class="m-menu__link-icon fa fa-users"></i><span class="m-menu__link-text">Segment Creator</span></a><div class="m-menu__submenu "><span class="m-menu__arrow"></span>
+           </li>
+            <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true"  m-menu-submenu-toggle="hover"><a href="#" class="m-menu__link m-menu__toggle"><i class="m-menu__link-icon fa fa-tasks"></i><span class="m-menu__link-text">Welcome Drip</span></a><div class="m-menu__submenu "><span class="m-menu__arrow"></span>
+           </li>
+            
+            	
+	</div>
+	<!-- END: Aside Menu -->
+</div>
+<!-- END: Left Aside -->							
+		   	
---
+++ views/send_notification_form.php
@@ -0,0 +1,233 @@
+<?php
+include ('header.php');?>		
+<!-- begin::Body -->
+<div class="m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-body">		
+<?php include('leftmenu.php'); ?>
+	
+<div class="m-grid__item m-grid__item--fluid m-wrapper">
+	<div class="m-subheader ">
+	<div class="d-flex align-items-center">
+ 		<div class="mr-auto">
+ 			<h3 class="m-subheader__title ">Send Push Notifications</h3>			
+		</div>
+  		</div>	
+  		</div>
+	<div class="m-portlet" id="send_notf_form">
+			<!--begin::Form-->
+			<form class="m-form m-form--fit m-form--label-align-right m-form--group-seperator-dashed">
+				<div class="m-portlet__body">	
+					<div class="form-group m-form__group row">
+						<div class="col-lg-6">
+							<label>Notification Title</label>
+							<textarea class="form-control m-input m-input--solid" id="exampleTextarea" rows="2" placeholder="96 characters max"></textarea>
+							</div>
+						<div class="col-lg-6">
+							<label class="">Contact Number:</label>
+							<input class="form-control m-input" placeholder="Enter contact number" type="email">
+							<span class="m-form__help">Please enter your contact number</span>
+						</div>
+					</div>	 
+					<div class="form-group m-form__group row">
+						<div class="col-lg-6">
+							<label>Message</label>
+							<div class="m-input-icon m-input-icon--right">
+								<textarea class="form-control m-input m-input--solid" id="exampleTextarea" rows="2" placeholder="255 characters max"></textarea>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<label class="">Postcode:</label>
+							<div class="m-input-icon m-input-icon--right">
+								<input class="form-control m-input" placeholder="Enter your postcode" type="text">
+								<span class="m-input-icon__icon m-input-icon__icon--right"><span><i class="la la-bookmark-o"></i></span></span>
+							</div>
+						</div>
+					</div>
+					<div class="form-group m-form__group row">
+						<div class="col-lg-6">
+							<label>Landing Page URL</label>
+							<div class="m-input-icon m-input-icon--right">
+								<input class="form-control m-input" placeholder="Enter URL with http:// or https://" type="text">
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<label class="">Postcode:</label>
+							<div class="m-input-icon m-input-icon--right">
+								<input class="form-control m-input" placeholder="Enter your postcode" type="text">
+								<span class="m-input-icon__icon m-input-icon__icon--right"><span><i class="la la-bookmark-o"></i></span></span>
+							</div>
+						</div>
+					</div>	
+						<div class="form-group m-form__group row">
+						<div class="col-lg-6">
+						<label>Image</label><div class="col-md-10"><div class="notification-min-height col-xs-3 p0"><img class="width-80 pull-left" src="../images/cpa.png" width="80"><div class="change-logo-text width-80 text-center cursor-pointer "><form enctype="multipart/form-data"><input accept="image/*" class="change-image-file cursor-pointer 
+            			" type="file"></form></div></div>
+            			<div></div><br><br><br>
+            			<p class="mb0"><span class="text-grey-color pt5 font-11 image-info-align block">Recommended Size: 192 X 192</span></p><p class="text-danger mt100 pos--absolute hide "></p></div>
+						</div>
+						<div class="col-lg-6">
+							<label class="">Postcode:</label>
+							<div class="m-input-icon m-input-icon--right">
+								<input class="form-control m-input" placeholder="Enter your postcode" type="text">
+								<span class="m-input-icon__icon m-input-icon__icon--right"><span><i class="la la-bookmark-o"></i></span></span>
+							</div>
+						</div>
+					</div>	 
+					<div class="form-group m-form__group row">
+						<div class="col-lg-6">
+							<div>
+								<label class="m-checkbox">
+											<input type="checkbox">Show Big Image in Notification
+											<span></span>
+											</label><br>
+											<span>Big size images are only supported on Chrome (56+) browser</span>
+											<div style="display: none; ">
+												<button type="button" class="btn btn-primary">Upload</button><br>
+												<span>Recommended Size: 600 X 400</span>
+											</div>
+							</div>
+						</div>
+					</div>	
+					<div class="form-group m-form__group row">
+						<div class="col-lg-6">
+							<div>
+								<label class="m-checkbox">
+											<input type="checkbox">Enable Button(s) on Chrome Notification
+											<span></span>
+											</label><br>
+											<span>Call to action buttons are only supported on Chrome browser</span><br><br>
+											<div style="visibility: ;">
+												<button type="button" class="btn btn-secondary">One button</button>  <button type="button" class="btn btn-primary">two buttons</button><br>
+											</div><br>
+											<div class="container" style="display: none;">
+												<label>Button text</label>
+												<input class="form-control m-input m-input--solid" id="exampleTextarea" rows="2" placeholder="36 characters max" type="text"><br>
+												<label>Landing Page URL</label>
+												<input class="form-control m-input m-input--solid" id="exampleTextarea" placeholder="Enter URL with http:// or https://"  type="text">
+											</div><br>
+												<div class="container" style="display: none;">
+												<label>Button text</label>
+												<input class="form-control m-input m-input--solid" id="exampleTextarea" rows="2" placeholder="36 characters max" type="text"><br>
+												<label>Landing Page URL</label>
+												<input class="form-control m-input m-input--solid" id="exampleTextarea" placeholder="Enter URL with http:// or https://"  type="text">
+											</div>
+							</div>
+						</div>
+					</div>	  
+					<div class="form-group m-form__group row">
+						<div class="col-lg-6">
+						<label class="">Send to</label><br>
+						<button type="button" class="btn btn-secondary ">All subscribers</button>
+						<button type="button" class="btn btn-primary ">Segments</button><br><br>
+						<div class="m-stack__demo-item">Notification will be sent to all 2 subscribers</div>
+						</div>
+					</div> 
+					<div class="form-group m-form__group row">
+						<div class="col-lg-6">
+						<label class="">Advanced Options</label><br>
+						<label class="m-checkbox">
+						<input type="checkbox">Auto-hide notification after 20 seconds.<i class="fa fa-question-circle"></i>
+						<span></span>
+						</label><br>
+						Don't attempt sending this notification
+						<div class="form-group m-form__group row" >
+							
+						<div class="inline-block col-lg-3"><label class="font-weight-400" for="days">Days</label>
+							<select class="form-control m-input" id="exampleSelect1" style="width: 47px;" >
+							<option>1</option>
+							<option>2</option>
+							<option>3</option>
+							<option>4</option>
+							<option>5</option>
+						</select><span style="text-align: left;">From Now</span>
+						</div>
+					
+					
+						<div class="inline-block inline-block col-lg-3"><label class="font-weight-400" for="days">Hours</label>
+							<select class="form-control m-input" id="exampleSelect1" style="width: 47px;" >
+							<option>1</option>
+							<option>2</option>
+							<option>3</option>
+							<option>4</option>
+							<option>5</option>
+						</select>
+						</div>
+						<div class="inline-block inline-block col-lg-3"><label class="font-weight-400" for="days">Minutes</label>
+							<select class="form-control m-input" id="exampleSelect1" style="width: 47px;" >
+							<option>1</option>
+							<option>2</option>
+							<option>3</option>
+							<option>4</option>
+							<option>5</option>
+						</select>
+						</div>
+
+						</div>
+					</div>
+					</div>  
+					<div class="form-group m-form__group row">
+						<div class="col-lg-6">
+							<div>
+								<label class="m-checkbox">
+											<input type="checkbox">Schedule Notification(Current time is 08-05-2018 15:10:59 GMT+03:00 Asia/Beirut)
+											<span></span>
+											</label><br>
+											
+							</div>
+							<div class="form-group m-form__group row" >
+							
+						<div class="inline-block col-lg-6">
+							<label class="font-weight-400" for="days">Date</label>
+							<div class="input-group date">
+						<input class="form-control m-input" readonly="" placeholder="Select date" id="m_datepicker_2" type="text">
+						<div class="input-group-append">
+							<span class="input-group-text">
+								<i class="la la-calendar-check-o"></i>
+							</span>
+						</div>
+					</div>
+						
+						</div>
+					
+					
+						<div class="inline-block inline-block col-lg-3"><label class="font-weight-400" for="days">Hours</label>
+							<select class="form-control m-input" id="exampleSelect1" style="width: 47px;" >
+							<option>1</option>
+							<option>2</option>
+							<option>3</option>
+							<option>4</option>
+							<option>5</option>
+						</select>
+						</div>
+						<div class="inline-block inline-block col-lg-3"><label class="font-weight-400" for="days">Minutes</label>
+							<select class="form-control m-input" id="exampleSelect1" style="width: 47px;" >
+							<option>1</option>
+							<option>2</option>
+							<option>3</option>
+							<option>4</option>
+							<option>5</option>
+						</select>
+						</div>
+
+						</div>
+						</div>
+					</div>	           
+	            </div>
+	            <div class="m-portlet__foot m-portlet__no-border m-portlet__foot--fit">
+					<div class="m-form__actions m-form__actions--solid">
+						<div class="row">
+							<div class="col-lg-6">
+								<button type="Submit" class="btn btn-primary btn-lg" style="margin-left: 30px;margin-bottom: 10px;">Send Notification</button>
+								
+							</div>
+							
+						</div>
+					</div>
+				</div>
+
+			</form>
+			<!--end::Form-->
+		</div>
+
+</div>
+</div>
+<?php include('footer.php'); ?>
